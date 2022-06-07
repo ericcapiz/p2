2aa5db7439a92b7c8178fe62c700528d9425a8c1
eric.capiz <eric.capiz@nextechsol.com>
2022-06-07 00:46:25 -0500

    done

--- components/About.jsx
+++ components/About.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const About = () => {
   return (
     <div id="about" className="w-full md:h-screen p-2 flex items-center py-16">
-      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+      <div className="max-w-[1240px] mt-20 m-auto md:grid grid-cols-3 gap-8">
         <div className="col-span-2">
           <p className="uppercase text-xl tracing-widest text-[#1D359D] ">
             About

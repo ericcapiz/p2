6fd42bcaac801fc1120b9570dd43f00085af6ac2
eric.capiz <eric.capiz@nextechsol.com>
2022-06-07 00:42:31 -0500

    updated styling

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -5,7 +5,7 @@ const Skills = () => {
   return (
     <div id="skills" className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
-        <p className="text-xl tracking-widest uppercase text-[#1D359D]">
+        <p className="text-xl mt-20  tracking-widest uppercase text-[#1D359D]">
           Skills
         </p>
         <h2 className="py-4">What I Can Do</h2>

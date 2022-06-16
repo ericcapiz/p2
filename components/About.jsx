b2307e10a299c53a216dd1296a810ac194858266
eric.capiz <eric.capiz@nextechsol.com>
2022-06-16 15:46:11 -0500

    fixed typo

--- components/About.jsx
+++ components/About.jsx
@@ -11,29 +11,30 @@ const About = () => {
           </p>
           <h2 className="py-4">Who I Am</h2>
           <p className="py-2 text-gray-600">
-            Becoming a deloper has always been my top choice for a career path.
-            Being able to see my creation come to life is an incredible feeling.
-            After Spending time in retail/sales as well as being in the
-            military, I decided to persue that dream. Now with every line of
-            code & every component created, that dream has come true.
+            Becoming a web developer has always been my top choice regarding a
+            career path. Being able to create web applications and watch it come
+            to life is an incredible feeling. After Spending time in
+            retail/sales as well as being in the military, I decided to persue
+            that dream. Now with every line of code & every component created,
+            that dream comes true!
           </p>
           <p className="py-2 text-gray-600">
-            It&apos;s the pixel perfect, experience I take pride on to ensure
-            users are getting the most out of their experience. With HTML, CSS,
-            to React and Javascript, and sprinkle in some Node.js, MongoDB, I
-            have a wide range of skills to build anything. As i like to say,
-            it&apos;s not what you build, but how you build it!
+            It&apos;s the pixel perfect, experience I take pride in to ensure
+            users are getting the most out of their experience. Between HTML,
+            CSS, to React and Javascript, and sprinkle in some Node.js, MongoDB,
+            I have a wide range of skills and abilities to build anything. As I
+            like to say, it&apos;s not what you build, but how you build it!
           </p>
           <Link href="/#projects">
             <p className="py-2 text-gray-600 underline cursor-pointer">
-              Check out some of my latest projects!
+              View Latest Projects!
             </p>
           </Link>
         </div>
-        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
+        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex p-4 hover:scale-105 ease-in duration-300">
           <img
             className="rounded-xl"
-            src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTB8fGNvZGluZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+            src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTB8fGNvZGluZ3xlbnwwfHwwfHw%3D&w=500&q=60"
             alt="/"
           />
         </div>
